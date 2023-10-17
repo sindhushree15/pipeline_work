6829dfa2771c217cfb14bf1a664ffa92cddedb14
sindhushree15 <sindhushree.15@gmail.com>
2023-10-17 14:16:09 +0530

    Update math.py

--- src/math.py
+++ src/math.py
@@ -1,13 +1,37 @@
-from src.math import add, subtract, multiply
+def add(x: int, y: int) -> int:
+    """add two numbers together
+
+    Args:
+                    x (int): first number to add
+                    y (int): second number to add
+
+    Returns:
+                    int: sum of x and y
+    """
+    return x + y
 
 
-def test_add():
-    assert add(3, 2) == 5
+def subtract(x: int, y: int) -> int:
+    """subtract one number from another
+
+    Args:
+                    x (int): first number to subtract
+                    y (int): second number to subtract
+
+    Returns:
+                    int: resut of x subtract y
+    """
+    return x - y
 
 
-def test_subtract():
-    assert subtract(3, 2) == 1
+def multiply(x: int, y: int) -> int:
+    """multiply two numbers together
 
+    Args:
+                    x (int): first number in the multiplication
+                    y (int): second number in the multiplication
 
-def test_multiply():
-    assert multiply(3, 2) == 6
+    Returns:
+                    int: product of x and y
+    """
+    return x * y
