4bef8486dff75cf65370f06bc34942a929c2057c
sindhushree15 <sindhushree.15@gmail.com>
2023-11-14 13:53:17 +0530

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -66,7 +66,7 @@ if [ $? -eq 0 ]; then
     echo "🔥🔥🔥🔥Security check passed🔥🔥🔥🔥"
 else
     echo "🔥🔥🔥🔥Security check failed🔥🔥🔥🔥"
-    cat $GITHUB_WORKSPACE/output/security_report.txt
+    cat $GITHUB_WORKSPACE/output/security_report.json
     if $INPUT_IGNORE_FAILURE; then
         exit 0
     else
