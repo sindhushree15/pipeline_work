bb8b88c97b83a94adf96cc79d925060fd75fa240
sindhushree15 <sindhushree.15@gmail.com>
2023-11-14 14:16:47 +0530

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -64,7 +64,8 @@ bandit -r $INPUT_PROJECT_PATH $INPUT_LEVEL $INPUT_CONFIDENCE -o $GITHUB_WORKSPAC
 
 if [ $? -eq 0 ]; then
     echo "🔥🔥🔥🔥Security check passed🔥🔥🔥🔥"
-    cat $GITHUB_WORKSPACE/output/security_report.txt >> $GITHUB_OUTPUT
+    $GITHUB_OUTPUT=`cat $GITHUB_WORKSPACE/output/security_report.txt`
+    echo "$GITHUB_OUTPUT"
 else
     echo "🔥🔥🔥🔥Security check failed🔥🔥🔥🔥"
     cat $GITHUB_WORKSPACE/output/security_report.txt >> $GITHUB_OUTPUT
